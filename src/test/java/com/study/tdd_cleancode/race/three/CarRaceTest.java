09b5c2c0619ca242568db3828e82b983a0ffbb0f
SYrongK <sYrongK@gmail.com>
2021-12-01 00:20:42 +0900

    docs: readme 생성

---
+++ src/test/java/com/study/tdd_cleancode/race/three/CarRaceTest.java
@@ -0,0 +1,17 @@
+package com.study.tdd_cleancode.race.three;
+
+public class CarRaceTest {
+
+    /*
+    * n대의 자동차
+    * 동작 : 전진 또는 정지 (랜덤)
+    * 사용자 입력 : 몇대의 자동차. 몇번 이동할지 (Scanner 사용)
+    * 전진 조건 : 랜던값이 0 ~ 9 중 4이상일 경우
+    * 화면 출력 : 자동차의 상태.(동작에 대한 상태이려나)
+    *
+    * UI 로직과 핵심 로직 구분
+    * UI 로직 : InputView, ResultView 클래스
+    *
+    * else 예약어 쓰지 않는다. (switch/case도 사용 x)
+    * */
+}
